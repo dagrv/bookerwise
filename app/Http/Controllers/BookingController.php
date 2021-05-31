73dc3d6115e60ae1ddfa469815a0117bb8895bd0
Rust <rust.91@icloud.com>
2021-05-31 14:07:45 +0200

    Generate Time Slots

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -2,12 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Bookings\TimeSlotGenerator;
+use App\Models\Schedule;
+use Carbon\CarbonInterval;
 use Illuminate\Http\Request;
 
 class BookingController extends Controller
 {
     public function __invoke()
     {
-        return view('bookings.create');
+        $schedule = Schedule::find(3);
+
+        $slots = (new TimeSlotGenerator($schedule))->get();
+
+        return view('bookings.create', [
+            'slots' => $slots
+        ]);
     }
 }
--- resources/views/bookings/create.blade.php
+++ resources/views/bookings/create.blade.php
@@ -1,1 +1,3 @@
-CREATE...
+@foreach($slots as $slot)
+    {{ $slot }} <br>
+@endforeach
