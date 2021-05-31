ed92d72456edbcf8b95d42bf1ebaef1cf974eb10
Rust <rust.91@icloud.com>
2021-05-31 14:15:06 +0200

    Filter the end of day (cannot book longer)

--- app/Bookings/TimeSlotGenerator.php
+++ app/Bookings/TimeSlotGenerator.php
@@ -3,6 +3,7 @@
 namespace App\Bookings;
 
 use App\Models\Schedule;
+use App\Models\Service;
 use Carbon\CarbonInterval;
 
 class TimeSlotGenerator
@@ -11,12 +12,14 @@ class TimeSlotGenerator
 
     protected $interval;
 
-    public function __construct(Schedule $schedule)
+    public function __construct(Schedule $schedule, Service $service)
     {
         $this->interval = CarbonInterval::minutes(self::INCREMENT)
             ->toPeriod(
                 $schedule->date->setTimeFrom($schedule->start_time),
-                $schedule->date->setTimeFrom($schedule->end_time)
+                $schedule->date->setTimeFrom(
+                    $schedule->end_time->subMinutes($service->duration)
+                )
             );
     }
 
--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Bookings\TimeSlotGenerator;
 use App\Models\Schedule;
+use App\Models\Service;
 use Carbon\CarbonInterval;
 use Illuminate\Http\Request;
 
@@ -12,8 +13,9 @@ class BookingController extends Controller
     public function __invoke()
     {
         $schedule = Schedule::find(3);
+        $service = Service::find(2);
 
-        $slots = (new TimeSlotGenerator($schedule))->get();
+        $slots = (new TimeSlotGenerator($schedule, $service))->get();
 
         return view('bookings.create', [
             'slots' => $slots
