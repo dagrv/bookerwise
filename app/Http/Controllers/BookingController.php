c3147535439655b89adf6542c9998d24c3db8d37
Rust <rust.91@icloud.com>
2021-06-02 16:01:25 +0200

    Schedule Unavailable Slots (Lunch time, etc..)

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -8,17 +8,19 @@ use App\Models\Service;
 use Carbon\CarbonInterval;
 use Illuminate\Http\Request;
 use App\Bookings\Filters\SlotsPassedTodayFilter;
+use App\Bookings\Filters\UnavailabilityFilter;
 
 class BookingController extends Controller
 {
     public function __invoke()
     {
         $schedule = Schedule::find(3);
-        $service = Service::find(2);
+        $service = Service::find(1);
 
         $slots = (new TimeSlotGenerator($schedule, $service))
             ->applyFilters([
-                new SlotsPassedTodayFilter()
+                new SlotsPassedTodayFilter(),
+                new UnavailabilityFilter($schedule->unavailabilities)
             ])
             ->get();
 
