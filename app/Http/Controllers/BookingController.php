aa3ee971f8af29ad07feedb8a5b066cecee4b1af
Rust <rust.91@icloud.com>
2021-06-03 00:08:31 +0200

    Appointment Filter

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Bookings\Filters\AppointmentFilter;
 use App\Bookings\TimeSlotGenerator;
 use App\Models\Schedule;
 use App\Models\Service;
@@ -9,6 +10,7 @@ use Carbon\CarbonInterval;
 use Illuminate\Http\Request;
 use App\Bookings\Filters\SlotsPassedTodayFilter;
 use App\Bookings\Filters\UnavailabilityFilter;
+use App\Models\Appointment;
 
 class BookingController extends Controller
 {
@@ -16,11 +18,13 @@ class BookingController extends Controller
     {
         $schedule = Schedule::find(3);
         $service = Service::find(1);
+        $appointments = Appointment::where('date', '2021-05-30')->get();
 
         $slots = (new TimeSlotGenerator($schedule, $service))
             ->applyFilters([
                 new SlotsPassedTodayFilter(),
-                new UnavailabilityFilter($schedule->unavailabilities)
+                new UnavailabilityFilter($schedule->unavailabilities),
+                new AppointmentFilter($appointments)
             ])
             ->get();
 
